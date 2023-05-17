c43908323fb8a8d033f795acaebffa9a1661c2a8
Denzil <denzil92@gmail.com>
2023-05-17 12:21:13 +0200

    Version 2.2

--- Project/script.js
+++ Project/script.js
@@ -48,7 +48,7 @@ rememberMyFilms();
 
 function detectPersonalLevel() {
   if (personalMovieDB.count < 10) {
-    alert('You haven\'t watched a lot of movies');
+    alert("You haven't watched a lot of movies");
   } else if (10 <= personalMovieDB.count && personalMovieDB.count <= 30) {
     alert('You are classic viewer');
   } else if (personalMovieDB.count > 30) {
@@ -72,9 +72,7 @@ function writeYourGenres() {
   let s = true;
   while (s) {
     let i = personalMovieDB.genres.length + 1;
-    let newGenre = prompt(`Your ${i}-st/d favorite genre`);
-    personalMovieDB.genres[i-1] = newGenre;
-    i++;
+    personalMovieDB.genres[i - 1] = prompt(`Your ${i}-st/d favorite genre`);
     s = confirm('Add new genre?');
   }
 }
